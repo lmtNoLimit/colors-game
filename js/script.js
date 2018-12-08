fa90977c23c994b44818e3903d0ffbf6c0b66031
Lâm Minh Thành <lmt151099@gmail.com>
2018-12-08 17:30:44 +0700

    Update new content

--- js/script.js
+++ js/script.js
@@ -1,28 +1,45 @@
-let colors = [
-    'rgb(255, 0, 255)',
-    'rgb(255, 0, 0)',
-    'rgb(0, 255, 0)',
-    'rgb(0, 0, 255)',
-    'rgb(255, 255, 0)',
-    'rgb(0, 255, 255)'
-];
-const pickedColor = colors[Math.round(Math.random()*6)];
+const colors = generateRandomColors(6);
+const pickedColor = colors[Math.round(Math.random() * colors.length)];
 const squares = document.querySelectorAll('.square');
-
+const msgDisplay = document.getElementById('message');
 // display picked color
 const colorDisplay = document.getElementById('color-display');
+const backgroundTitle = document.querySelector('.title');
+
 colorDisplay.textContent = pickedColor;
 colorDisplay.classList.add('pickedColor');
+
+function changeColor(color){
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.background = color;
+    }
+}
 
 for(let i = 0; i < squares.length; i++){
     squares[i].style.background = colors[i];
     squares[i].addEventListener('click', function(){
         let clickedColor = this.style.background;
         if(clickedColor === pickedColor){
-            console.log('Correct');
+            changeColor(clickedColor);
+            msgDisplay.textContent = 'Correct!';
+            backgroundTitle.style.background = clickedColor;          
         } else {
-            console.log('Try Again');
+            this.style.background = '#232323';
+            msgDisplay.textContent = 'Try Again!!!';
         }
     });
 }
+function randomColor() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 
+function generateRandomColors(x) {
+    const arr = [];
+    for(let i = 0; i < x; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
