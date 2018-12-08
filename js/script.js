2d989be237718f20fe22453c2ba016c3f75370da
Lâm Minh Thành <lmt151099@gmail.com>
2018-12-08 18:47:34 +0700

    New Update

--- js/script.js
+++ js/script.js
@@ -1,41 +1,18 @@
-const colors = generateRandomColors(6);
-const pickedColor = colors[Math.round(Math.random() * colors.length)];
-const squares = document.querySelectorAll('.square');
-const msgDisplay = document.getElementById('message');
+let colors = generateRandomColors(6);
+let pickedColor = colors[Math.round(Math.random() * colors.length)];
+let squares = document.querySelectorAll('.square');
+let msgDisplay = document.getElementById('message');
 // display picked color
-const colorDisplay = document.getElementById('color-display');
-const backgroundTitle = document.querySelector('.title');
+let colorDisplay = document.getElementById('color-display');
+let backgroundTitle = document.querySelector('.title');
+let resetBtn = document.getElementById('reset');
 
-colorDisplay.textContent = pickedColor;
-colorDisplay.classList.add('pickedColor');
-
-function changeColor(color){
-    for(let i = 0; i < squares.length; i++){
-        squares[i].style.background = color;
-    }
-}
-
-for(let i = 0; i < squares.length; i++){
-    squares[i].style.background = colors[i];
-    squares[i].addEventListener('click', function(){
-        let clickedColor = this.style.background;
-        if(clickedColor === pickedColor){
-            changeColor(clickedColor);
-            msgDisplay.textContent = 'Correct!';
-            backgroundTitle.style.background = clickedColor;          
-        } else {
-            this.style.background = '#232323';
-            msgDisplay.textContent = 'Try Again!!!';
-        }
-    });
-}
 function randomColor() {
     let r = Math.floor(Math.random()*256);
     let g = Math.floor(Math.random()*256);
     let b = Math.floor(Math.random()*256);
     return `rgb(${r}, ${g}, ${b})`;
 }
-
 function generateRandomColors(x) {
     const arr = [];
     for(let i = 0; i < x; i++){
@@ -43,3 +20,36 @@ function generateRandomColors(x) {
     }
     return arr;
 }
+function changeColor(color){
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.background = color;
+    }
+}
+colorDisplay.textContent = pickedColor;
+colorDisplay.classList.add('pickedColor');
+for(let i = 0; i < squares.length; i++){
+    squares[i].style.background = colors[i];
+    squares[i].addEventListener('click', function(){
+        let clickedColor = this.style.background;
+        if(clickedColor === pickedColor){
+            changeColor(clickedColor);
+            msgDisplay.textContent = 'Correct!';
+            backgroundTitle.style.background = clickedColor;
+            resetBtn.textContent = 'Play Again?';
+        } else {
+            this.classList.toggle('shake');
+            msgDisplay.textContent = 'Try Again!!!';
+        }
+    });
+}
+resetBtn.addEventListener('click', function(){
+    colors = generateRandomColors(6);
+    pickedColor = colors[Math.round(Math.random() * colors.length)];
+    colorDisplay.textContent = pickedColor;
+    backgroundTitle.style.background = '#4c8bf7';
+    resetBtn.textContent = 'New Colors';
+    msgDisplay.textContent = null;
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+});
