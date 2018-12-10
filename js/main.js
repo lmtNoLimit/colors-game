fbbe04c298de3dd823420848f0c11111b6f0a40e
Lâm Minh Thành <lmt151099@gmail.com>
2018-12-10 21:46:14 +0700

    Fix generate color not display

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ function randomColor() {
     let r = Math.round(Math.random()*255);
     let g = Math.round(Math.random()*255);
     let b = Math.round(Math.random()*255);
-    console.log(`rgb(${r}, ${g}, ${b})`);
     return `rgb(${r}, ${g}, ${b})`;
 }
 function generateRandomColors(x) {
